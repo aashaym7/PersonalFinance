921484564f0e78690eda621afa28056f9ef8e0be
aashaym7 <aashaymehta6@gmail.com>
2023-07-22 18:27:50 -0400

    Add google sheet integration API (follow instructions on https://docs.gspread.org/en/latest/oauth2.html#enable-api-access-for-a-project)

--- financeManager.py
+++ financeManager.py
@@ -1,4 +1,5 @@
 import csv
+import gspread
 
 MONTH = 'July'
 
@@ -8,3 +9,6 @@ with open(file, mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
         print(row)
+
+sa = gspread.service_account()
+sh = sa.open("Personal Finances")
