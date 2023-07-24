fa9eb9d9bb371d28524c97120f187760dd2f29d9
aashaym7 <aashaymehta6@gmail.com>
2023-07-24 19:23:17 -0400

    Add basic template and assigned variables to each row of csv file

--- financeManager.py
+++ financeManager.py
@@ -1,14 +1,25 @@
 import csv
 import gspread
+# from decimal import Decimal
 
 MONTH = 'July'
 
 file = f"Amex_{MONTH}.csv"
 
+transactions = []
+
 with open(file, mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
+    headers = next(csv_file) 
     for row in csv_reader:
-        print(row)
-
+        date = row[0]
+        name = row[1]
+        # amount = Decimal(row[2])
+        # amountInt = row[2].replace(" ", "")
+        # print(amountInt)
+        # amount = float(amountInt)
+        amount = float(row[2])
+        category = 'other'
+        transaction = ((date, name, amount, category))
 sa = gspread.service_account()
 sh = sa.open("Personal Finances")
